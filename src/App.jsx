9a448b2009b8e8a0aa18c23ecc094fb772000324
hamzarazaucp <hamza123raza@gmail.com>
2025-02-26 15:08:02 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App()
+{
+  return <h1>
+    Yesss
+  </h1>
+}
+export default App;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
